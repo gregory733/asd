2d1fdd7d38ea96803d124d12c7d4067d7c7bd986
gregory733 <plan90gg@bk.ru>
2024-03-01 21:12:57 +0300

    les 3.6

--- Test_project/sitewomen/sitewomen/settings.py
+++ Test_project/sitewomen/sitewomen/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'women.apps.WomenConfig',
-
     'django_extensions',
+
+    'women.apps.WomenConfig',
 ]
 
 MIDDLEWARE = [
